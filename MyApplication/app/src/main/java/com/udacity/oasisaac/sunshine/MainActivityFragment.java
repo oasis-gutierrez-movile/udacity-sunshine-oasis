17ae16e4076aa484649438fbd6d85dcb855811cb
Oasis Gutierrez <oasis.gutierrez@movile.com>
2015-11-24 22:04:46 -0600

    List View

--- MyApplication/app/src/main/java/com/udacity/oasisaac/sunshine/MainActivityFragment.java
+++ MyApplication/app/src/main/java/com/udacity/oasisaac/sunshine/MainActivityFragment.java
@@ -1,10 +1,16 @@
 package com.udacity.oasisaac.sunshine;
 
+import android.content.res.Resources;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -18,6 +24,24 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View view =  inflater.inflate(R.layout.fragment_main, container, false);
+        List<String> list = new ArrayList<String>();
+        list.add("Today");
+        list.add("Tomorrow");
+        list.add("Monday");
+        list.add("Tuestday");
+        list.add("Wendsday");
+        list.add("Thursday");
+        list.add("Friday");
+
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(getActivity(),R.layout.list_item_forecast,R.id.list_item_forecast_textview,list);
+
+        ListView listView = (ListView) view.findViewById(R.id.listview_forecast);
+        listView.setAdapter(adapter);
+
+
+        return view;
     }
 }
